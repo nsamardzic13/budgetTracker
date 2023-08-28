cc18a8fe3b6eacc4012a805987fdacb010403825
nsamardzic13 <nikola.samardzic1997@gmail.com>
2023-08-28 20:26:20 +0200

    Minor changes

--- execute.sh
+++ execute.sh
@@ -8,10 +8,10 @@ git pull
 TODAY=$(date +%Y-%m-%d)
 HTML_FILE="output.html"
 IPYNB_FILE="main.ipynb"
-SUBJECT="Report for ${TODAY}"
+SUBJECT="Budget Report for ${TODAY}"
 
 source .venv/bin/activate 
 jupyter nbconvert --output ${HTML_FILE}  --no-input --to=HTML --execute ${IPYNB_FILE}
-python send_email.py --subject $SUBJECT --filename ${HTML_FILE}
+python send_email.py --subject "${SUBJECT}" --filename "${HTML_FILE}"
 
 rm -f ${HTML_FILE}
