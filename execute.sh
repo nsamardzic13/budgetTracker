96cd4dbc556a078cf05fff16d4c261daae141149
nsamardzic13 <nikola.samardzic1997@gmail.com>
2023-08-28 19:53:59 +0200

    Init commit

---
+++ execute.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+set -eo pipefail
+
+TODAY=$(date +%Y-%m-%d)
+HTML_FILE="output.html"
+IPYNB_FILE="main.ipynb"
+SUBJECT="Report for ${TODAY}"
+
+source .venv/bin/activate 
+jupyter nbconvert --output ${HTML_FILE}  --no-input --to=HTML --execute ${IPYNB_FILE}
+python send_email.py --subject $SUBJECT --filename ${HTML_FILE}
+
+rm -f ${HTML_FILE}
